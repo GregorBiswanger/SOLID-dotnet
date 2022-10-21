ad1bd3d19443c82a551cc29d2b04bb687a638853
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2022-10-22 00:19:20 +0200

    Add Microsoft Dependency Injection

--- PlzSuperTool/App.xaml.cs
+++ PlzSuperTool/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using PlzSuperTool.Infrastructure.Features.MainWindow;
 
 namespace PlzSuperTool
 {
@@ -13,5 +15,20 @@ namespace PlzSuperTool
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var serivceCollection = new ServiceCollection();
+            serivceCollection.AddTransient<MainWindowViewModel>();
+
+            _serviceProvider = serivceCollection.BuildServiceProvider(true);
+
+            Current.MainWindow = new MainWindow();
+            Current.MainWindow.DataContext = _serviceProvider.GetService<MainWindowViewModel>();
+            Current.MainWindow.Show();
+        }
     }
 }
